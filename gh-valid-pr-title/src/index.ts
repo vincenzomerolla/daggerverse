7821d7bc46f16811f768c0ef3e372fcd904f9275
Vincenzo Merolla <vmerolla920@gmail.com>
2026-04-23 15:41:20 -0400

    fix: update deps and remove dep on dagger mod

--- gh-valid-pr-title/src/index.ts
+++ gh-valid-pr-title/src/index.ts
@@ -1,12 +1,6 @@
 import config from "@commitlint/config-conventional";
 import lint from "@commitlint/lint";
-import {
-  type Container,
-  dag,
-  func,
-  object,
-  type Secret,
-} from "@dagger.io/dagger";
+import { dag, func, object, type Secret } from "@dagger.io/dagger";
 // @ts-expect-error
 import createPreset from "conventional-changelog-conventionalcommits";
 
@@ -20,26 +14,24 @@ export class GhValidPrTitle {
     repo: string,
     branch: string,
     githubToken: Secret,
-  ): Promise<Container> {
+  ): Promise<string> {
     let container = dag
-      .gh({
-        repo,
-        token: githubToken,
-      })
-      .exec(["pr", "view", branch, "--json", "title"]);
+      .container()
+      .withEnvVariable("GH_REPO", repo)
+      .withSecretVariable("GITHUB_TOKEN", githubToken)
+      .from("serversideup/github-cli:2.91.0")
+      .withExec(["gh", "pr", "view", branch, "--json", "title"]);
     let pr = JSON.parse(await container.stdout());
     let parserPreset = await createPreset({});
     let report = await lint(pr.title, config.rules, {
       parserOpts: parserPreset.parserOpts,
     });
 
-    console.log("Commitlint report:", report);
-
     if (!report.valid) {
       let errors = report.errors.map((e) => `- ${e.message}`).join("\n");
       throw new Error(`\nFailed to pass commitlint:\n\n${errors}`);
     }
 
-    return container;
+    return `✅ PR title "${pr.title}" passes commitlint conventional commit rules`;
   }
 }
